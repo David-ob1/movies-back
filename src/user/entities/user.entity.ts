24966738a4d9bd0adfc0deeb4bfc68f55efbd625
David-ob1 <davidvazquezt2001@gmail.com>
2024-08-15 13:22:39 -0300

    User

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,25 @@
-export class User {}
+import { Delete } from "@nestjs/common";
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("user")
+export class User {
+    @PrimaryGeneratedColumn("increment")
+    id:number
+
+    @Column("text")
+    name:string
+
+    @Column("text")
+    email:string
+
+    @Column("text")
+    password: string
+
+    @CreateDateColumn()
+    crate_at: Date;
+
+    @DeleteDateColumn()
+    deleted_at:string
+
+
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
+
+  constructor (
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ){}
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return this.userRepository.save(createUserDto)
   }
 
   findAll() {
+    return this.userRepository.find()
     return `This action returns all user`;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.userRepository.findOne({where: {id:id}})
+
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+
+    return this.userRepository.update({id:id},updateUserDto)
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    return this.userRepository.softDelete({id:id});
   }
+
+  hardDelete(id: number){
+    return this.userRepository.delete({id:id})
+  }
+
+  
 }
